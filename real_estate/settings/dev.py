2649e185c372fb7bddcbbfb3015a62afc06fd9bd
Abhik Das <abhikdas98765@gmail.com>
2022-08-13 09:53:48 +0530

    separated the dev and prod settings and modifed urlpatterns

---
+++ real_estate/settings/dev.py
@@ -0,0 +1,13 @@
+from .base import *
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': env("DB_ENGINE"),
+        'NAME': env("DB_NAME"),
+        'USER': env("DB_USER"),
+        'PASSWORD': env("DB_PASSWORD"),
+        'HOST': env("DB_HOST"),
+        'PORT': env("DB_PORT"),
+    }
+}
